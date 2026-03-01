f4b06d897b63b8d287e36f6775bf17efe8f8c645
metachasm-beep <metachasm@gmail.com>
2026-03-02 04:34:52 +0530

    fix: revert next.config.ts to avoid build TypeError and enable standard Cloudflare build pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import { withCloudflare } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withCloudflare(nextConfig);
-
-if (process.env.NODE_ENV === 'development') {
-  import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
-}
+export default nextConfig;
