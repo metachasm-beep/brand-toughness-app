9a813bd65b12cc3eea5919e6ee16de93a19ae7aa
metachasm-beep <metachasm@gmail.com>
2026-03-14 05:50:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'standalone',
   serverExternalPackages: ['playwright', 'lighthouse', 'chrome-launcher'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
